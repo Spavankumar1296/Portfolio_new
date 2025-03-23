d796f65716ad7de13e0401b4e72a398ab2ba63a8
pavan <pavankumarseepana1210@gmail.com>
2025-03-23 22:05:42 +0530

    modified NavBar,Projects Section,and Make Responsive

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -79,12 +79,6 @@ const Navbar = () => {
                   href="#"
                   className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-blue-300 hover:text-black"
                 >
-                  Team
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-blue-300 hover:text-black"
-                >
                   Projects
                 </a>
                 <a
@@ -99,88 +93,17 @@ const Navbar = () => {
 
           {/* Notification and profile dropdown */}
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
+           <a href="mailto:pavankumarseepana1210@gmail.com">
+           <button
               type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white  hover:bg-blue-600"
+              className="relative rounded-full bg-white p-1 text-gray-400 "
             >
               <span className="absolute -inset-1.5"></span>
-              <span className="sr-only">View notifications</span>
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                />
-              </svg>
+              <span className="sr-only">Contact Me</span>
+              <img className="h-6 w-6" src="https://img.icons8.com/fluency/48/mail--v1.png" alt="mail--v1"/>
             </button>
+           </a>
 
-            {/* Profile dropdown
-            <div className="relative ml-3">
-              <div>
-                <button
-                  type="button"
-                  className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  id="user-menu-button"
-                  aria-expanded="false"
-                  aria-haspopup="true"
-                  onClick={toggleProfileDropdown}
-                >
-                  <span className="absolute -inset-1.5"></span>
-                  <span className="sr-only">Open user menu</span>
-                  <img
-                    className="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </button>
-              </div> */}
-
-              {/* Dropdown menu
-              {isProfileDropdownOpen && (
-                <div
-                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="user-menu-button"
-                  tabIndex="-1"
-                >
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="user-menu-item-0"
-                  >
-                    Your Profile
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="user-menu-item-1"
-                  >
-                    Settings
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="user-menu-item-2"
-                  >
-                    Sign out
-                  </a>
-                </div>
-              )} */}
-            {/* </div> */}
           </div>
         </div>
       </div>
@@ -191,26 +114,20 @@ const Navbar = () => {
           <div className="space-y-1 px-2 pt-2 pb-3">
             <a
               href="#"
-              className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-black"
+              className="block px-3 py-2 text-base font-medium text-black  hover:text-black"
               aria-current="page"
             >
-              Dashboard
+              About
             </a>
             <a
               href="#"
-              className="block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-gray-700 hover:text-white"
-            >
-              Team
-            </a>
-            <a
-              href="#"
-              className="block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-gray-700 hover:text-white"
+              className="block rounded-md px-3 py-2 text-base font-medium text-black  hover:text-black"
             >
               Projects
             </a>
             <a
               href="#"
-              className="block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-gray-700 hover:text-white"
+              className="block rounded-md px-3 py-2 text-base font-medium text-black  hover:text-black"
             >
               Calendar
             </a>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,51 +6,59 @@ const projects = [
     title: "Gemini Clone",
     imgUrl: "https://camo.githubusercontent.com/5e533895d4d4e2e2466e1836cb48e7d800705ac7f6a898695d44660bbddb3e1f/68747470733a2f2f6c68332e676f6f676c6575736572636f6e74656e742e636f6d2f6544723670594b7331745430694b306e743370506876566c5032576e39366662477162576742414152525a376973656a303337675f74576f626a56387a516b784f73577a4a75454838702d666b73637a58554f657178475a5a496f5f485543646b6e38712d61346675774154443751395872733d77323435362d6c3130302d73672d726a2d633078666666666666",
     description:" The application is developed using React and the Gemini API, featuring a responsive,user-friendly interface. ",
+    Repository:"https://github.com/Spavankumar1296/Gemini_Clone",
+    live:"https://gemini-clone-fuh30q78a-s-pavan-kumars-projects-c9c4d3e0.vercel.app/",
   },
   {
     title: "Food Much Restaurant",
     imgUrl: "https://img.freepik.com/free-photo/view-table-with-articles-food-family_1398-5025.jpg?t=st=1740674941~exp=1740678541~hmac=413e32d29e8a50510152342093e0498cd96037082eaeb1b641dcb5b26965e117&w=1380",
     description: "Designed and developed a fully responsive website for a Food Much Restaurant,focusing on user engagement and accessibility. ",
+    Repository:"https://github.com/Spavankumar1296/Static-Restaurant-website-using-HTML-CSS",
+    live:"https://spavankumar1296.github.io/Static-Restaurant-website-using-HTML-CSS/",
   },
   {
-    title: "weather Appliaction",
+    title: "Weather Appliaction",
     imgUrl: "https://img.freepik.com/free-vector/realistic-weather-widget_1284-4092.jpg?t=st=1740675293~exp=1740678893~hmac=4d07c2a02be1e9022f50b8bc01b6fd268d6c9fa46aef5abac104228a709538b2&w=900",
     description: " Built an interactive weather application that provides real-time weather updates based on user location. ",
+    Repository:"https://github.com/Spavankumar1296/Static-Restaurant-website-using-HTML-CSS",
+    live:"https://spavankumar1296.github.io/Weather-application/",
   },
 ];
 
 const projects_ml = [
   {
-    title: "ML Project 1",
-    imgUrl: "https://img.freepik.com/premium-vector/programing-concept-illustration_353829-161.jpg?w=826",
-    description: "This is a machine learning project.",
-  },
-  {
-    title: "ML Project 2",
-    imgUrl: "https://img.freepik.com/premium-vector/programing-concept-illustration_353829-161.jpg?w=826",
-    description: "This is a machine learning project.",
-  },
-  {
-    title: "ML Project 3",
-    imgUrl: "https://img.freepik.com/premium-vector/programing-concept-illustration_353829-161.jpg?w=826",
-    description: "This is a machine learning project.",
+    title: "Covid 19 Prediction ",
+    imgUrl: "https://img.freepik.com/premium-vector/doctor-persons-with-protective-suit-spraying-covid-19-disinfection-virus-concept_24877-64161.jpg?w=996",
+    description: "This project is to develop a predictive model that can forecast COVID-19 cases, deaths, or recovery rates based on historical and real-time data .",
+    Repository:"https://github.com/Spavankumar1296/COVID-19-ML-MODEL",
+    live:"https://covid-19-ml-model-qki8qnuqe99yu558j8ny4u.streamlit.app/",
   },
 ];
+
+const projects_java=[
+  {
+    title:"Bharat Gas Agency",
+    imgUrl:"https://img.freepik.com/free-photo/environmental-pollution-factory-exterior_23-2149057714.jpg?t=st=1742742420~exp=1742746020~hmac=e88a41284df091445e85d5076b331197afd09d26e077289520e565669c5bfa4b&w=1380",
+    description:"The app offers a user-friendly interface for customers, agents, and gas agencies. Comprehensive documentation aids installation, usage, and troubleshooting.",
+    Repository:"",
+    live:"",
+  },
+]
 
 const Projects = () => {
   const [selectedProjects, setSelectedProjects] = useState(projects);
 
   return (
     <div className="h-screen w-screen">
-      <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4 m-3 justify-center">
-        <button className="btn btn-primary" onClick={() => setSelectedProjects(projects)}>
-           Web Projects
+      <div className="flex flex-col md:flex-row lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4 m-3 justify-center">
+        <button className="buttons" onClick={() => setSelectedProjects(projects)}>
+           WEB PROJECTS
         </button>
-        <button className="btn btn-secondary" onClick={() => setSelectedProjects(projects_ml)}>
-          ML Projects
+        <button className="buttons" onClick={() => setSelectedProjects(projects_ml)}>
+          ML PROJECTS
         </button>
-        <button className="btn btn-secondary" onClick={() => setSelectedProjects(projects_ml)}>
-          Java Projects
+        <button className="buttons" onClick={() => setSelectedProjects(projects_java)}>
+          JAVA PROJECTS
         </button>
       </div>
       <div className="container">
@@ -60,10 +68,12 @@ const Projects = () => {
             <img className='img-src  w-full object-fit' src={project.imgUrl} alt="Project" />
             </div>
             <div>
-              <h1 className="text-2xl lg:ml-6  text-gray-900 dark:text-white font-semibold">{project.title}</h1>
-              <p className="text-lg text-black lg:ml-6 dark:text-white">{project.description}</p>
-              
-              <button className="btn btn-primary mt-4 lg:ml-6">Click Me</button>
+              <h1 className="md:text-2xl sm:text-xl md:ml-6 mt-3 mb-1 text-gray-900 dark:text-white font-semibold">{project.title}</h1>
+              <p className="md:text-lg sm:text-sm text-gray-600 md:ml-6 mb-1 dark:text-white">{project.description}</p>
+              <div className= "md:ml-6 md:mt-3">
+                <a href={project.Repository}><button className="btn-pro mt-4 ">Code</button></a>
+                <a href={project.live}><button className="btn-pro mt-4 ">View</button></a>
+              </div>
             </div>
           </div>
         ))}
